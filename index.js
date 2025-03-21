efdd2e097e83e0f0a138ecc97d7852ac34fccefa
David O'reilly <davidworeilly@gmail.com>
2025-03-21 12:48:59 +1100

    prompt moved to worker

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 const email = document.getElementById("email-preview")
 
 async function fetchChatWorker() {
-    const messages = [
-        {
-            role: 'system',
-            content: 'You are Braze customer engagment platform email expert'
-        },
-        {
-            role: 'user',
-            content: `Write me email copy for the video editing platform Clipchamp presuading me to try it. It needs to be in the following format:
-            
-            Heading: 10 words of less
-            Body copy: 60 words or less
-            CTA: 5 words or less
-            
-            The copy should be upbeat, engaging and aimed at novice information workers. It should also include specific references to Clipchamp features to avoid sounding generic.
-            Do not include introductions such as "hello there"`
-        }
-    ]
+    // const userMessage = 
+
     
     try {
         const url = 'https://openai-api-worker.davidworeilly.workers.dev'
@@ -27,26 +12,26 @@ async function fetchChatWorker() {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(messages)
+            body: JSON.stringify({})
         })
         const data = await response.json()
         
         if (!response.ok) {
             throw new Error(`Worker Error: ${data.error}`)
         }
-        console.log(data.content)
-        const emailToSplit = data.content
-        const output = emailToSplit.split(/: |\n/)
-        console.log(output)
+        console.log(data)
+        // const emailToSplit = data.content
+        // const output = emailToSplit.split(/: |\n/)
+        // console.log(output)
         
-        renderEmail(output)
+        renderEmail(data)
 
     } catch (err) {
         console.error(err.message)
     }
 }
 
-fetchChatWorker()
+// fetchChatWorker()
 
 function renderEmail(elements) {
     const headingElement = document.getElementById("heading-el");
